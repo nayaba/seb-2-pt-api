2d0e372f6792e799149c076619c6950ff7638545
Nabila Ayaba <nabilaayaba@gmail.com>
2025-01-29 16:51:32 +0300

    Update index

--- index.js
+++ index.js
@@ -73,6 +73,6 @@ app.delete('/students/:id', async (req, res) => {
   }
 })
 
-// Start Server
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
